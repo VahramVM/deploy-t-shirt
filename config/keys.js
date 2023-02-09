a1868dc98d779aa1b7c848eea1b12f4c13b0f1ed
VahramVM <vahram.keleshyan@gmail.com>
2023-02-09 20:48:52 +0400

    test34

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Vahram1:120485@cluster0-p3qfx.mongodb.net/T_short?retryWrites=true&w=majorityyy",
+    mongoURI: "mongodb+srv://Vahram111:120485@cluster0-p3qfx.mongodb.net/T_short?retryWrites=true&w=majority",
     jwt: 'dev-jwt'
 }
 
