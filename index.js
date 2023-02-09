a15852aea4b17d2e06e2ea0298043b9c938368bc
VahramVM <vahram.keleshyan@gmail.com>
2023-02-09 15:20:46 +0400

    test24

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 // const appMail = require('./mailer')
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
