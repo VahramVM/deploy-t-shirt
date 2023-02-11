af909ccc80f54c004c0d91fc4e686c4bece2fb10
VahramVM <vahram.keleshyan@gmail.com>
2023-02-11 15:30:20 +0400

    test56

--- client/build/main-es2015.js
+++ client/build/main-es2015.js
@@ -4113,7 +4113,7 @@ class AuthServices {
         return this.http.get('api/category').pipe();
     }
     register(user) {
-        return this.http.post('http://localhost:5000/api/auth/register', user);
+        return this.http.post('https://t-shop.cyclic.app/api/auth/register', user);
     }
     login(user) {
         return this.http.post('https://t-shop.cyclic.app/api/auth/login', user).
