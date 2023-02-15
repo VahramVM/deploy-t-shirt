fab1f6fc13f0861b7017c9b83ff23c6299f55bad
VahramVM <vahram.keleshyan@gmail.com>
2023-02-15 18:42:58 +0400

    test78

--- routes/category.js
+++ routes/category.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/category');
 const upload = require('../middleweare/upload')
 
 //localhost:5000/api/category
-router.get('/', passport.authenticate('jwt', {session: false}), controller.getByName)
+router.get('/', passport.authenticate('jwt', {session: true}), controller.getAll)
 router.get('/:id',  controller.getAll)
 router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove)
 router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create)
